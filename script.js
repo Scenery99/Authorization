369821eb377a40d201f3d23bfa110ac82a2e2426
Scenery99 <petimatkhankerkhanova951@gmail.com>
2024-12-15 21:50:17 +0300

    проблема

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.backgroundImage = "url('img/фон2.png')";
+    document.body.style.backgroundPosition = "button center";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+}
